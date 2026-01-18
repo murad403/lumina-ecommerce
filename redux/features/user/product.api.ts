6b9e5acd4660dea6ec72f40c7548a14e04284267
murad403 <mdm78774@gmail.com>
2026-01-18 09:55:28 +0600

    feat: add product API endpoints for fetching products and product details; implement query logic and export hooks

---
+++ redux/features/user/product.api.ts
@@ -0,0 +1,29 @@
+import baseApi from "@/redux/api/baseApi";
+
+const productApi = baseApi.injectEndpoints({
+    endpoints: (builder) =>({
+        getProducts: builder.query({
+            query: ({search, category, min_price, max_price}) =>{
+                const params = new URLSearchParams();
+                if(search) params.append('search', search);
+                if(category) params.append("category", category);
+                if(min_price) params.append("min_price", min_price.toString());
+                if(max_price) params.append("max_price", max_price.toString());
+                return {
+                    url: `/products/products/?${params.toString()}`,
+                    method: "GET"
+                }
+            }
+        }),
+        productDetails: builder.query({
+            query: ({slug}) =>{
+                return {
+                    url: `/products/products/${slug}/`,
+                    method: "GET"
+                }
+            }
+        }),
+    })
+})
+
+export const { useGetProductsQuery, useProductDetailsQuery } = productApi;
